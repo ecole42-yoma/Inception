760ddc86851305bccc0dc71d1cf2659b0485a113
Yongman Kim <yongmkim@c1r5s6.42seoul.kr>
2022-11-07 19:17:48 +0900

    [edit] env file

--- srcs/requirements/wordpress/conf/configuration.sh
+++ srcs/requirements/wordpress/conf/configuration.sh
@@ -42,13 +42,13 @@ then
     wp-cli core download --path=$WORDPRESS_PATH
 
     entrypoint_log "wp-cli config create 🔍 "
-    wp-cli config create --dbname=$WORDPRESS_DB_NAME --dbuser=$WORDPRESS_DB_USER --dbpass=$WORDPRESS_DB_PASSWORD --dbhost=$BACK_NETWORK --path=$WORDPRESS_PATH
+    sudo wp-cli config create --dbname=$WORDPRESS_DB_NAME --dbuser=$WORDPRESS_DB_USER --dbpass=$WORDPRESS_DB_PASSWORD --dbhost=$BACK_NETWORK --path=$WORDPRESS_PATH
 
     entrypoint_log "wp-cli core install 🔍 "
-    wp-cli core install --url=https://$DOMAIN_NAME --title=$WORDPRESS_TITLE --admin_user=$WORDPRESS_ADMIN_USER --admin_password=$WORDPRESS_ADMIN_PASSWORD --admin_email=$WORDPRESS_ADMIN_EMAIL --skip-email --path=$WORDPRESS_PATH
+    sudo wp-cli core install --url=https://$DOMAIN_NAME --title=$WORDPRESS_TITLE --admin_user=$WORDPRESS_ADMIN_USER --admin_password=$WORDPRESS_ADMIN_PASSWORD --admin_email=$WORDPRESS_ADMIN_EMAIL --skip-email --path=$WORDPRESS_PATH
 
     entrypoint_log "wp-cli user create 🔍 "
-    wp-cli user create $WORDPRESS_USER $WORDPRESS_EMAIL --role=$WORDPRESS_USER_ROLE --user_pass=$WORDPRESS_USER_PASSWORD --path=$WORDPRESS_PATH
+    sudo wp-cli user create $WORDPRESS_USER $WORDPRESS_EMAIL --role=$WORDPRESS_USER_ROLE --user_pass=$WORDPRESS_USER_PASSWORD --path=$WORDPRESS_PATH
 else
 # already installed
     entrypoint_log "$ME: already installed wordpress : try update"
