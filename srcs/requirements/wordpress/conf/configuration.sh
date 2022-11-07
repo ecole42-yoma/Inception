918d83d90938ac017162902c654c8d88ab123702
Yongman Kim <yongmkim@c1r5s6.42seoul.kr>
2022-11-07 19:37:49 +0900

    [edit] configure.sh on wordpress

--- srcs/requirements/wordpress/conf/configuration.sh
+++ srcs/requirements/wordpress/conf/configuration.sh
@@ -35,8 +35,8 @@ check_error "$ME: setting default conf : /etc/php8/php-fpm.d/www.conf"
 
 
 entrypoint_log "$ME: install wordpress, setting 🔍 "
-if [ $(find $WORDPRESS_PATH -follow -type f -print | wc -l) -eq 0 ]
-then
+# if [ $(find $WORDPRESS_PATH -follow -type f -print | wc -l) -eq 0 ]
+# then
 
     entrypoint_log "wp-cli core download 🔍 "
     wp-cli core download --path=$WORDPRESS_PATH
@@ -49,16 +49,16 @@ then
 
     entrypoint_log "wp-cli user create 🔍 "
     wp-cli user create $WORDPRESS_USER $WORDPRESS_EMAIL --role=$WORDPRESS_USER_ROLE --user_pass=$WORDPRESS_USER_PASSWORD --path=$WORDPRESS_PATH
-else
-# already installed
-    entrypoint_log "$ME: already installed wordpress : try update"
+# else
+#     # already installed
+#     entrypoint_log "$ME: already installed wordpress : try update"
 
-    entrypoint_log "wp-cli core update 🔍 "
-    wp-cli core update --path=$WORDPRESS_PATH
+#     entrypoint_log "wp-cli core update 🔍 "
+#     wp-cli core update --path=$WORDPRESS_PATH
 
-    entrypoint_log "wp-cli core update-db 🔍 "
-    wp-cli core update-db --path=$WORDPRESS_PATH
-fi
+#     entrypoint_log "wp-cli core update-db 🔍 "
+#     wp-cli core update-db --path=$WORDPRESS_PATH
+# fi
 check_error "$ME: install wordpress, setting"
 
 
