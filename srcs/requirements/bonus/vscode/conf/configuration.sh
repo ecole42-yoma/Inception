1d2108b8b1058adb3f7f2cfdc1399464ab7e1d27
Yongman Kim <yongmkim@c1r3s5.42seoul.kr>
2022-11-10 18:23:13 +0900

    [edit] vescode..

--- srcs/requirements/bonus/vscode/conf/configuration.sh
+++ srcs/requirements/bonus/vscode/conf/configuration.sh
@@ -24,14 +24,32 @@ check_error() {
 
 
 entrypoint_log "$ME: setup vscode 🔍 "
-wget https://github.com/cdr/code-server/releases/download/v3.3.1/code-server-3.3.1-linux-amd64.tar.gz
-tar -xzvf code-server-3.3.1-linux-amd64.tar.gz
-cp -r code-server-3.3.1-linux-amd64 /usr/lib/code-server
-ln -s /usr/lib/code-server/bin/code-server /usr/bin/code-server
-mkdir /var/lib/code-server
+mkdir -p /app/code-server
+
+wget    https://github.com/coder/code-server/releases/download/v4.8.3/code-server-4.8.3-linux-amd64.tar.gz
+tar xf code-server-4.8.3-linux-amd64.tar.gz -C /app/code-server --strip-components=1
+
+# wget    https://github.com/coder/code-server/releases/download/v4.8.3/code-server-4.8.3-linux-arm64.tar.gz
+# tar xf code-server-4.8.3-linux-arm64.tar.gz -C /app/code-server --strip-components=1
+
+# wget    https://github.com/coder/code-server/releases/download/v4.8.3/code-server-4.8.3-linux-armv7l.tar.gz
+# tar xf code-server-4.8.3-linux-armv7l.tar.gz -C /app/code-server --strip-components=1
+
+
+
+ln -s /app/code-server/bin/code-server /usr/bin/code-server
+
+
+# mkdir /var/lib/code-server
+
+# sh /app/code-server/node
+
+echo "*****************"
+
 
 # wget -O- 'https://aka.ms/install-vscode-server/setup.sh' | sh
 # curl -fsSL https://code-server.dev/install.sh | sh
+
 
 # check_error "$ME: setup vscode"
 
